ad8f9c9d497fc765dcb53e40ab2a122545e5d16b
Nabiha Wasim <nabihawasim1508@gmail.com>
2026-08-15 21:08:35 +0530

    Used static keyword to give same values in objects

---
+++ 06_Class_Objects/static_object.java
@@ -0,0 +1,10 @@
+public class static_object {
+    public static void main(String[] args) {
+        static_class s1=new static_class();
+        static_class s2=new static_class();
+        s1.setDetails("Harvey",34);
+        s2.setDetails("Enola",35);
+        s1.displayDetails();
+        s2.displayDetails();  
+    }  
+}
