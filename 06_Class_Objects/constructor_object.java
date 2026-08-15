8d0291872566028bfc5ca6fd23d074eedaefc196
Nabiha Wasim <nabihawasim1508@gmail.com>
2026-08-15 21:22:17 +0530

    Used constructors from class file to object file

---
+++ 06_Class_Objects/constructor_object.java
@@ -0,0 +1,9 @@
+public class constructor_object {
+    public static void main(String[] args) {
+        constructor_class s1=new constructor_class("Enola",23, 34,"A");
+        constructor_class s2=new constructor_class("Sherlock",34 ,890 ,"D");
+        s1.displayinfo();
+        System.out.println();
+        s2.displayinfo();
+    }
+}
