da1b8356c82ea477ea69739ac62e8165b21d26e9
Nabiha Wasim <nabihawasim1508@gmail.com>
2026-08-18 13:36:48 +0530

    jhbcuheb

---
+++ 08_String_Class/final_keyword.java
@@ -0,0 +1,18 @@
+//Create an object with 
+// final fields and a constructor to initialize them.
+public class final_keyword {
+    String name;
+    final int key;
+    final int id;
+    public final_keyword(String name,int key,int id){
+        this.name=name;
+        this.key=key;
+        this.id=id;
+        //cannot change now key and id because they are in final
+        //keyword
+
+    }
+
+
+    
+}
