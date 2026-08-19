28d06020e41003613e78dce96348e75bbb7355d8
Nabiha Wasim <nabihawasim1508@gmail.com>
2026-08-19 23:35:45 +0530

    rhbf

---
+++ 09_Encapsulation/package_geometry/circle.java
@@ -0,0 +1,13 @@
+package package_geometry;
+public class circle {
+    private double radius;
+    public circle(double radius){
+        this.radius=radius;
+    }
+    public void setradius(double radius){
+        this.radius=radius;
+    }
+    public double getradius(){
+        return radius;
+    } 
+}
