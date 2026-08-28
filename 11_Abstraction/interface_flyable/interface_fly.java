f4c21b9faab40235890a0926089665f14013793f
Nabiha Wasim <nabihawasim1508@gmail.com>
2026-08-28 11:59:07 +0530

    Used  interface

---
+++ 11_Abstraction/interface_flyable/interface_fly.java
@@ -0,0 +1,8 @@
+/*Create an interface Flyable with an abstract method fly().
+Create an abstract class Bird that implements Flyable.
+Implement a subclass Eagle that extends Bird. Provide an 
+implementation for the fly() method. */
+package interface_flyable;
+interface interface_fly {
+     void fly();  
+}
