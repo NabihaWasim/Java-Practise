22719874aa6ce173e365fce3ef935c4d1013700a
Nabiha Wasim <nabihawasim1508@gmail.com>
2026-08-28 12:00:54 +0530

    Used  interface

---
+++ 11_Abstraction/interface_flyable/testclass.java
@@ -0,0 +1,7 @@
+package interface_flyable;
+public class testclass {
+    public static void main(String[] args) {
+        eagle_class eagle=new eagle_class("Francis", null);
+        eagle.fly();
+    }
+}
