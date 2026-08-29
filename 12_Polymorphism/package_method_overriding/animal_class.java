1c8d2aeed9646fda053164f1692dd561c93a67ef
Nabiha Wasim <nabihawasim1508@gmail.com>
2026-08-29 21:06:31 +0530

    Implemented method oerriding

---
+++ 12_Polymorphism/package_method_overriding/animal_class.java
@@ -0,0 +1,9 @@
+package package_method_overriding;
+public class animal_class {
+    public void sound(){
+        System.out.println("Animal is shouting");
+    }
+    public void eat(){
+        System.out.println("is eating");
+    }
+}
